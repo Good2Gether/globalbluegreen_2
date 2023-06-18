3ead527393d9e4db1f9eb157fe4dd07f748e0029
Rima-Maria Rahal <rahal.rimamaria@gmail.com>
2023-06-18 18:55:30 +0200

    toloka ready

--- public/src/main.js
+++ public/src/main.js
@@ -64,7 +64,7 @@
         document.body.style.cursor = 'auto';
         var randomCode = jsPsych.randomization.randomID(7);
         jsPsych.endExperiment(`<div>
-        Your survey code is: <b>Cog${randomCode}Glo27<b></b></br>
+        Your survey code is: <b>92830102<b></b></br>
         You can close the browser to end the experiment now. </br>
                     The webcam will turn off when you close the browser. </br>
                      
@@ -119,7 +119,7 @@
     var start_exp_survey_trial = {
       type: jsPsychSurveyText,
       questions: [
-        {prompt: "What's your MTURK Worker ID (needed for bonus payment)?", rows: 2, columns:50 , required:true}, 
+        {prompt: "What's your toloka ID (needed for bonus payment)?", rows: 2, columns:50 , required:true}, 
         {prompt: "What's your age?", rows: 1, columns: 50, required:true},
         {prompt: "What's your gender? (Female/Male/Other)", rows: 1, columns: 50,require: true},
       ],
@@ -1316,7 +1316,7 @@
            })
            .done(function () {
             ("The experiment data has been saved successfully!" +
-            "Your survey code is: <b> C8GNYXQW." +
+            "Your survey code is: <b> 92830102." +
             "You can close the browser to end the experiment now. " +
             "The webcam will turn off when you close the browser.")
            })
